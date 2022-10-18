701ee09d401f4351e91ac368db839363458fe528
SERGIOCH2023 <sergio6ifran@gmail.com>
2022-10-18 18:45:31 -0300

    Disparos
    Disparo de la nave players

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -10,6 +10,9 @@ export var potencia_rotacion:int = 280
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int =0
 
+## Atributos Onready
+onready var canion:Canion = $Canion
+
 ##Metodos
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
@@ -34,6 +37,12 @@ func player_input() -> void:
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
+	
+	#Disparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
 
 
 

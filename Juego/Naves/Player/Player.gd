6c9ed514c5bf7397338f3c1f30dc36cbb8c0ea01
SERGIOCH2023 <sergio6ifran@gmail.com>
2022-10-22 10:50:58 -0300

    Muerte Explosión
    Muerte Explosión

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -5,31 +5,56 @@ extends RigidBody2D
 ##Atributos Export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
+export var estela_maxima:int = 150
 
 ## Atributos
+var estado_actual:int = ESTADO.SPAWN
 var empuje:Vector2 = Vector2.ZERO
-var dir_rotacion:int =0
+var dir_rotacion:int = 0
 
 ## Atributos Onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
+onready var estela:Estela = $EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
+
+## Enums
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
 
 ##Metodos
+func _ready() -> void:
+	controlador_estados(estado_actual)
+	
 func _unhandled_input(event: InputEvent) -> void:
+	if not esta_input_activo():
+		return
 	#Disparo Rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
 		
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
-		
 
-func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	
+	
+	#Control Estela
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(0)
+		motor_sfx.sonido_on()
+	
+	if (event.is_action_released("mover_adelante") or event.is_action_released("mover_atras")):
+		motor_sfx.sonido_off()
+	
+func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 	
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	player_input()
 
 ## Metodos Custom
@@ -40,6 +65,8 @@ func player_input() -> void:
 		empuje = Vector2(potencia_motor, 0)
 	elif Input.is_action_pressed("mover_atras"):
 		empuje = Vector2(-potencia_motor, 0)
+	if not esta_input_activo():
+		return
 		
 	#Rotacion
 	dir_rotacion=0
@@ -54,5 +81,37 @@ func player_input() -> void:
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
 
+
+func controlador_estados(nuevo_estado: int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled",false)
+			canion.set_puede_disparar(false)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled",true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled",true)
+			canion.set_puede_disparar(true)
+			Eventos.emit_signal("nave_destruida",global_position,3)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	
+	estado_actual = nuevo_estado
+
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO,ESTADO.SPAWN]:
+		return false
+	return true
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "spawn":
+		controlador_estados(ESTADO.VIVO)
+
+func destruir() -> void:
+	controlador_estados(ESTADO.MUERTO)
 
 

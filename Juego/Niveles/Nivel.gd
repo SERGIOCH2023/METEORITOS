1f700bc4a6dc4fdd487d91cd0fcfc066301ad3ef
SERGIOCH2023 <sergio6ifran@gmail.com>
2022-10-27 20:34:22 -0300

    Pulido del juego - semana 3
    -se agrego mas animaciones de destrucción en meteoritos
    -Se agrego un mejor fondo
    -pulido en la cámara destrucción player

--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -48,12 +48,20 @@ func crear_contenedores() -> void:
 	add_child(contenedor_sector_meteoritos)
 
 func _on_disparo(proyectil:Proyectil) -> void:
-	add_child(proyectil)
+	contenedor_proyectiles.add_child(proyectil)
 
-func _on_nave_destruida(posicion: Vector2, num_explosiones: int) -> void:
+func _on_nave_destruida(nave: Player, posicion: Vector2, num_explosiones: int) -> void:
+	if nave is Player:
+		transicion_camaras(
+			posicion,
+			posicion+crear_posicion_aleatoria(-200.0,200.0),
+			camara_nivel,
+			tiempo_transicion_camara
+		)
+		
 	for _i in range(num_explosiones):
 		var new_explosion:Node2D = explosion.instance()
-		new_explosion.global_position = posicion
+		new_explosion.global_position = posicion+crear_posicion_aleatoria(100.0,50.0)
 		add_child(new_explosion)
 		yield(get_tree().create_timer(0.6),"timeout")
 
@@ -120,6 +128,15 @@ func transicion_camaras(
 	)
 	camara_actual.current = true
 	$TweenCamara.start()
+
+func crear_posicion_aleatoria(rango_horizontal: float, rango_vertical:float)->Vector2:
+	randomize()
+	var rand_x = rand_range(-rango_horizontal, rango_horizontal)
+	var rand_y = rand_range(-rango_vertical, rango_vertical)
+	
+	return Vector2(rand_x, rand_y)
+
+##coneccion Señales externas
 
 
 

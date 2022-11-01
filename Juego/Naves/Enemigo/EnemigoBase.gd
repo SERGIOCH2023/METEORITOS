fb1706c4cdda6ef6bc8329728e55177bf6122301
SERGIOCH2023 <sergio6ifran@gmail.com>
2022-10-31 21:13:05 -0300

    Pulido fin semana 4

--- Juego/Naves/Enemigo/EnemigoBase.gd
+++ Juego/Naves/Enemigo/EnemigoBase.gd
@@ -5,13 +5,17 @@ extends NaveBase
 # Atributos
 var player_objetivo:Player = null
 var dir_player:Vector2
+var frame_actual: int = 0
+
 ## Metodos
 func _ready() -> void:
 	player_objetivo = DatosJuego.get_player_actual()
 	Eventos.connect("nave_destruida",self,"_on_nave_destruida")
 
 func _physics_process(delta: float) -> void:
-	rotar_hacia_player()
+	frame_actual += 1
+	if frame_actual % 3 == 0:
+		rotar_hacia_player()
 
 ## Metodos Custom
 func _on_nave_destruida(nave: NaveBase, _posicion, _explosiones)-> void:
